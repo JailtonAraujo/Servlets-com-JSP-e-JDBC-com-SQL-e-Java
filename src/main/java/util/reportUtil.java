aa6598b58f3de8ead767ffc548285593fb2f466f
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-03-14 22:15:58 -0300

    Criação de relatorio de usuario

---
+++ src/main/java/util/reportUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import jakarta.servlet.ServletContext;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+@SuppressWarnings({"rawtypes","unchecked"})
+public class reportUtil implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	public byte [] gerarRelatorioPDF (List listaDados, String nomeRelatorio, ServletContext context) throws Exception {
+		
+		/*CRIANDO A LISTA DE DADOS VINDA DA CONSULTA SQL*/
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(listaDados);
+		
+		String caminhoJasper = context.getRealPath("relatorio") + File.separator + nomeRelatorio + ".jasper";
+		
+		JasperPrint impressoraJasper = JasperFillManager.fillReport(caminhoJasper, new HashedMap(), dataSource);
+		
+		return JasperExportManager.exportReportToPdf(impressoraJasper);
+		
+		
+	}
+
+}
